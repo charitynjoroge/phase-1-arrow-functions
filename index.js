102c705d698252673b9b7cffc3dea07231bd96b0
Charity Njoroge <charity.njoroge@student.moringaschool.com>
2024-03-19 21:49:30 +0300

    complete lab

---
+++ index.js
@@ -0,0 +1,14 @@
+const divide = (a,b) => (a/b);
+divide(2000,100);
+let a = 2000
+let b = 100
+
+const square = (x) => (x*x);
+square(2);
+let x = 2
+
+const add = (A,B) => (A+B);
+add(3,4);
+let A = 3
+let B = 4
+
